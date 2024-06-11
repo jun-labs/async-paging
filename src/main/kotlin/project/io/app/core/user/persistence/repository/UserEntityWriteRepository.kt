782dd567d3671efbc5d40d8aa12b9a793fef70ea
devjun10 <devjun1023@gmail.com>
2024-06-11 18:55:52 +0900

    refactor: 테스트를 위한 @Transactional 추가

--- src/main/kotlin/project/io/app/core/user/persistence/repository/UserEntityWriteRepository.kt
+++ src/main/kotlin/project/io/app/core/user/persistence/repository/UserEntityWriteRepository.kt
@@ -2,6 +2,7 @@ package project.io.app.core.user.persistence.repository
 
 import jakarta.persistence.EntityManager
 import org.springframework.stereotype.Repository
+import org.springframework.transaction.annotation.Transactional
 import project.io.app.core.user.domain.User
 import project.io.app.core.user.persistence.UserWriteRepository
 import project.io.app.logger
@@ -13,6 +14,11 @@ class UserEntityWriteRepository(
 
     private val log = logger()
 
+    /**
+     * 테스트를 위한 메서드.
+     * 실제 환경이라면 @Transactional 제거할 것.
+     * */
+    @Transactional
     override fun saveAll(users: List<User>) {
         users.forEach { user ->
             entityManager.persist(user)
